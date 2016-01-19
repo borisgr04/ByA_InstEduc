a3cbbea23a0c18fe4135f4cea101138782ea0189
Adrian Vergara <adrianvergara22@gmail.com>
2016-01-19 12:00:26 -0500

    Quite mapper de SignalR

--- webInstEduc/Startup.cs
+++ webInstEduc/Startup.cs
@@ -21,25 +21,6 @@ namespace AspIdentity
             });*/
             ConfigureAuth(app);
             // Any connection or hub wire up and configuration should go here
-            app.Map("/signalr", map =>
-            {
-                // Setup the CORS middleware to run before SignalR.
-                // By default this will allow all origins. You can 
-                // configure the set of origins and/or http verbs by
-                // providing a cors options with a different policy.
-                map.UseCors(CorsOptions.AllowAll);
-                var hubConfiguration = new HubConfiguration
-                {
-                    // You can enable JSONP by uncommenting line below.
-                    // JSONP requests are insecure but some older browsers (and some
-                    // versions of IE) require JSONP to work cross domain
-                    // EnableJSONP = true
-                };
-                // Run the SignalR pipeline. We're not using MapSignalR
-                // since this branch already runs under the "/signalr"
-                // path.
-                map.RunSignalR(hubConfiguration);
-            });
         }
     }
 }
